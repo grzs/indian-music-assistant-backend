52e3af9bdb6c216935eb5302146c0147ea16ac48
Janos Gerzson <janos.gerzson@vertis.com>
2025-11-04 10:54:53 +0100

    [UPD] mongodb connection test

---
+++ mongodb-connection-test.py
@@ -0,0 +1,25 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+import json
+
+
+def connect(user, password, address, app_name):
+    uri = f"mongodb+srv://{user}:{password}@{address}/?appName={app_name}"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi("1"))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    with open("mongodb.json") as f:
+        credentials = json.load(f)
+
+    connect(**credentials)
